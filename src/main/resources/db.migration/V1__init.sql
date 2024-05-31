2939cd321eeb43c141b9b1502430f791fc3386d1
alirezapla <hajlialireza@gmail.com>
2024-06-01 00:30:00 +0330

    fix: update migrations

--- src/main/resources/db.migration/V1__init.sql
+++ src/main/resources/db.migration/V1__init.sql
@@ -1,84 +1,88 @@
-create table document
-(
-    id                      varchar(255) not null primary key,
-    created_date            date null,
-    created_by              varchar(128) null,
-    updated_date            date null,
-    updated_by              varchar(128) null,
-    is_hided                boolean,
-    is_deleted              boolean,
-    description             varchar(512),
-    doc_table_id            varchar(255),
-    computing_table_item_id varchar(255),
-    name                    varchar(128)
-);
-
-create table doc_table
+create table monitoring.document
 (
     id           varchar(255) not null primary key,
-    created_date date null,
+    created_date date         null,
+    created_by   varchar(128) null,
+    updated_date date         null,
+    updated_by   varchar(128) null,
+    is_hided     boolean,
+    is_deleted   boolean,
+    description  varchar(512),
+    name         varchar(128)
+);
+
+create table monitoring.doc_table
+(
+    id           varchar(255) not null primary key,
+    created_date date         null,
     created_by   varchar(128),
-    updated_date date null,
+    updated_date date         null,
     updated_by   varchar(128),
     is_hided     boolean,
     is_deleted   boolean,
     name         varchar(128),
-    indicator_id varchar(255)
+    document_id  varchar(256)
 );
-create table indicator
+create table monitoring.indicator
 (
     id           varchar(255) not null primary key,
-    created_date date null,
+    created_date date         null,
     created_by   varchar(128),
-    updated_date date null,
+    updated_date date         null,
     updated_by   varchar(128),
     is_hided     boolean,
     is_deleted   boolean,
     name         varchar(128),
     record_order int,
+    doc_table_id varchar(256),
     computation  JSONB
 );
 
-create table computing_table_items
+create table monitoring.computing_table_items
 (
     id           varchar(255) not null primary key,
-    created_date date null,
+    created_date date         null,
     created_by   varchar(128),
-    updated_date date null,
+    updated_date date         null,
     updated_by   varchar(128),
     is_hided     boolean,
     is_deleted   boolean,
     command      varchar(256),
-    description  varchar(512)
+    description  varchar(512),
+    document_id  varchar(256)
+
 
 );
 
 
-create table user_credentials
+create table monitoring.user_credentials
 (
     id          varchar(255) not null primary key,
-    create_date date null,
-    update_date date null,
-    firstname   varchar(255) null,
-    lastname    varchar(255) null,
     password    varchar(255) not null,
     username    varchar(255) not null
 );
 
-create table user_authority
+create table monitoring.user_authority
 (
     user_credential_id varchar(255) not null,
     user_authority     varchar(255) not null,
     primary key (user_credential_id, user_authority)
 );
 
-alter table user_credentials
+alter table monitoring.user_credentials
     add constraint UK_8pcilw7ay5rs8c4dtrtik21pw unique (username);
-alter table user_authority
+alter table monitoring.user_authority
     add constraint FK9eonwn7vhem2ho9n3oikh4c9s foreign key (user_credential_id) references user_credentials (id);
-alter table document
-    add constraint document_doc_table_fk foreign key (doc_table_id) references doc_table (id);
-alter table document
-    add constraint document_computing_table_items_fk foreign key (computing_table_item_id) references computing_table_item (id);
-alter table doc_table
-    add constraint doc_table_indicator_fk foreign key (indicator_id) references indicator (id);
+alter table monitoring.doc_table
+    add constraint doc_table_document_fk foreign key (document_id) references document (id);
+alter table monitoring.computing_table_items
+    add constraint document_computing_table_items_fk foreign key (document_id) references document (id);
+alter table monitoring.indicator
+    add constraint doc_table_indicator_fk foreign key (doc_table_id) references doc_table (id);
+
+INSERT INTO monitoring.user_credentials(id, username, password)
+VALUES ('9153ee25-9767-4d5a-bfc7-42db1ca0accf', 'admin',
+        '$2a$12$DKBWuNEGrVj0QS97YQBUdO6qIGyxq1PKQe1ygGKUG6S3T7zEt516y');
+
+INSERT INTO monitoring.user_authority(user_credential_id, user_authority)
+VALUES ('9153ee25-9767-4d5a-bfc7-42db1ca0accf', 'AUTHORITY_ADMIN');
