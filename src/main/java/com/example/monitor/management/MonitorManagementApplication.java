bcdc967d53405d1c39375778f7cdf1ae1c940fac
pla <adad@asdasd.csd>
2024-06-13 18:58:32 +0430

    fix: update

--- src/main/java/com/example/monitor/management/MonitorManagementApplication.java
+++ src/main/java/com/example/monitor/management/MonitorManagementApplication.java
@@ -1,13 +1,14 @@
 package com.example.monitor.management;
 
+import com.example.monitor.management.common.AppLogEvent;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.data.envers.repository.config.EnableEnversRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import com.example.monitor.management.common.MyLogger;
 
 
 @EnableJpaAuditing
@@ -17,6 +18,7 @@ import org.springframework.data.envers.repository.support.EnversRevisionReposito
 public class MonitorManagementApplication {
 
 	public static void main(String[] args) {
+		MyLogger.doLog(LogLevel.INFO, AppLogEvent.MONITORING_APP_STARTED,"");
 		SpringApplication.run(MonitorManagementApplication.class, args);
 	}
 
