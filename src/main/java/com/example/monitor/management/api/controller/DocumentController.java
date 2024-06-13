8c3ddd6130c8e497df10f5a26e75e5c8d387b5ba
pla <adad@asdasd.csd>
2024-06-13 18:55:37 +0430

    fix: update

--- src/main/java/com/example/monitor/management/api/controller/DocumentController.java
+++ src/main/java/com/example/monitor/management/api/controller/DocumentController.java
@@ -6,77 +6,78 @@ import com.example.monitor.management.api.utils.httputil.pagination.PaginationDT
 import com.example.monitor.management.api.utils.httputil.response.SuccessfulRequestResponseEntity;
 import com.example.monitor.management.common.AppLogEvent;
 import com.example.monitor.management.common.validators.BodyValidator;
+import com.example.monitor.management.common.validators.ValuesAllowed;
 import com.example.monitor.management.common.Dto.BodyDto;
 import com.example.monitor.management.common.MyLogger;
 import com.example.monitor.management.common.exceptions.BodyValidationException;
 import com.example.monitor.management.domain.service.DocumentService;
-import com.example.monitor.management.infrastructure.security.authentication.JwtAuthentication;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
 
+@Validated
 @RestController
 @RequestMapping(value = "documents")
 public class DocumentController {
     private final DocumentService documentService;
-    private final MyLogger logger;
 
-    public DocumentController(DocumentService documentService, MyLogger logger) {
+    public DocumentController(DocumentService documentService) {
 
         this.documentService = documentService;
-        this.logger = logger;
     }
 
     @PostMapping()
     public ResponseEntity<Object> createDocuments(@Valid @RequestBody BodyDto bodyDto,
                                                   BindingResult bindingResult, Authentication authentication) throws BodyValidationException {
-        logger.doLog(LogLevel.INFO, AppLogEvent.CREATE_REQUEST_RECEIVED, bodyDto);
+        MyLogger.doLog(LogLevel.INFO, AppLogEvent.CREATE_REQUEST_RECEIVED, bodyDto);
         BodyValidator.isValid(bodyDto, bindingResult);
-        return responseHandler(documentService.create(((JwtAuthentication) authentication).getUserDetails(), bodyDto), AppLogEvent.CREATE_RESPONSE_SENT);
+        return responseHandler(documentService.create((UserDetails) authentication.getPrincipal(), bodyDto), AppLogEvent.CREATE_RESPONSE_SENT);
     }
 
     @GetMapping(value = "/{docId}")
     public ResponseEntity<Object> getDocuments(
-            @RequestParam int page,
-            @RequestParam int perPage,
             @PathVariable String docId,
             @RequestParam(required = true) String clientPerspective
     ) {
-        logger.doLog(LogLevel.INFO, AppLogEvent.FETCH_REQUEST_RECEIVED, docId);
+        MyLogger.doLog(LogLevel.INFO, AppLogEvent.FETCH_REQUEST_RECEIVED, docId);
         return responseHandler(
-                documentService.retrieve(pageRequest(page, perPage), docId, clientPerspective),
+                documentService.retrieve(docId, clientPerspective),
                 AppLogEvent.FETCH_RESPONSE_SENT);
     }
 
     @GetMapping()
     public ResponseEntity<Object> getAllDocuments(
-            @RequestParam int page,
-            @RequestParam int perPage,
+            @RequestParam(required = true) int page,
+            @RequestParam(required = true) int perPage,
+            @RequestParam(required = false) @ValuesAllowed(values = {"name", "id"}) String orderBy,
             @RequestParam(required = false) String searchTerm) {
-        logger.doLog(LogLevel.INFO, AppLogEvent.FETCH_REQUEST_RECEIVED, "");
+        MyLogger.doLog(LogLevel.INFO, AppLogEvent.FETCH_REQUEST_RECEIVED, "");
         return responseHandler(
-                documentService.retrieveAll(pageRequest(page, perPage), searchTerm),
+                documentService.retrieveAll(pageRequest(page, perPage, orderBy), searchTerm),
                 AppLogEvent.FETCH_RESPONSE_SENT);
     }
 
     @PutMapping(value = "/{docId}")
     public ResponseEntity<Object> updateDocument(@PathVariable String docId, @Valid @RequestBody
     BodyDto bodyDto, BindingResult bindingResult, Authentication authentication) throws BodyValidationException {
-        logger.doLog(LogLevel.INFO, AppLogEvent.EDIT_REQUEST_RECEIVED, docId);
+        MyLogger.doLog(LogLevel.INFO, AppLogEvent.EDIT_REQUEST_RECEIVED, docId);
         BodyValidator.isValid(bodyDto, bindingResult);
-        return responseHandler(documentService.update(((JwtAuthentication) authentication).getUserDetails(), docId, bodyDto), AppLogEvent.EDIT_RESPONSE_SENT);
+        return responseHandler(documentService.update((UserDetails) authentication.getPrincipal(), docId, bodyDto), AppLogEvent.EDIT_RESPONSE_SENT);
     }
 
     @DeleteMapping(value = "/{docId}")
     public ResponseEntity<Object> removeDocument(@PathVariable String docId, Authentication authentication) throws IOException {
-        logger.doLog(LogLevel.INFO, AppLogEvent.DELETE_REQUEST_RECEIVED, docId);
-        return responseHandler(documentService.remove(((JwtAuthentication) authentication).getUserDetails(), docId), AppLogEvent.DELETE_RESPONSE_SENT);
+        MyLogger.doLog(LogLevel.INFO, AppLogEvent.DELETE_REQUEST_RECEIVED, docId);
+        return responseHandler(documentService.remove((docId)), AppLogEvent.DELETE_RESPONSE_SENT);
     }
 
     private ResponseEntity<Object> responseHandler(Object data, AppLogEvent event) {
@@ -85,8 +86,11 @@ public class DocumentController {
         );
     }
 
-    private PageRequest pageRequest(int page, int perPage) {
+    private PageRequest pageRequest(int page, int perPage, String orderBy) {
         PaginationDTO paginationDTO = new PaginationDTO(page, perPage);
+        if (!orderBy.isEmpty()) {
+            return PageRequest.of(paginationDTO.getPage() - 1, paginationDTO.getPerPage(), Sort.by(orderBy));
+        }
         return PageRequest.of(paginationDTO.getPage() - 1, paginationDTO.getPerPage());
     }
 }
